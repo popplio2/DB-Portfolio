7c6a70ea02c706a1b4e5514142b4885ae8e05df1
popplio2 <danikbrisk@yahoo.com>
2024-11-28 19:48:04 -0500

    switched theme icons

--- portfolio/src/components/ThemeButton.vue
+++ portfolio/src/components/ThemeButton.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="theme-btn" @click="toTheme()"><font-awesome-icon :icon="currentTheme" /></button>
+  <button class="theme-btn" @click="toTheme()">
+    <font-awesome-icon :icon="currentTheme" />
+  </button>
 </template>
 
 <script>
@@ -9,46 +11,46 @@ export default {
   },
   data() {
     return {
-      currentTheme: '',
-    }
+      currentTheme: "",
+    };
   },
   mounted() {
-    if(this.theme) {
+    if (this.theme) {
       this.currentTheme = this.theme;
     } else {
-      this.currentTheme = 'sun';
+      this.currentTheme = "moon";
     }
   },
   methods: {
     toTheme() {
-      if (this.currentTheme === 'sun') {
-      document.documentElement.setAttribute('theme', 'moon');
-      this.currentTheme = document.documentElement.getAttribute('theme');
+      if (this.currentTheme === "moon") {
+        document.documentElement.setAttribute("theme", "sun");
+        this.currentTheme = document.documentElement.getAttribute("theme");
       } else {
-        document.documentElement.setAttribute('theme', 'sun');
-        this.currentTheme = document.documentElement.getAttribute('theme');
+        document.documentElement.setAttribute("theme", "moon");
+        this.currentTheme = document.documentElement.getAttribute("theme");
       }
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
-  button {
-    z-index: 1;
-    position: fixed;
-    top: 0;
-    right: 0;
-    margin: 3%;
-    color: var(--button-text);
-    font-size: 1.9rem;
-    font-weight: 700;
-    padding: .75rem 1rem;
-    background-color: var(--button-bg);
-    border: .15rem solid var(--button-border);
-    border-radius: 10rem;
-    box-shadow: var(--box-shadow);
-    transition: all .3s;
-    cursor: pointer;
-  }
+button {
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  right: 0;
+  margin: 3%;
+  color: var(--button-text);
+  font-size: 1.9rem;
+  font-weight: 700;
+  padding: 0.75rem 1rem;
+  background-color: var(--button-bg);
+  border: 0.15rem solid var(--button-border);
+  border-radius: 10rem;
+  box-shadow: var(--box-shadow);
+  transition: all 0.3s;
+  cursor: pointer;
+}
 </style>
